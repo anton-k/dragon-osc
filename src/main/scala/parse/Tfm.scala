ea39708196d9f86f49b96f0bb7fe9df3710bbc9f
anton-k <anton.kholomiov@gmail.com>
2016-11-16 16:48:41 +0300

    init include from files

--- src/main/scala/parse/Tfm.scala
+++ src/main/scala/parse/Tfm.scala
@@ -1,1 +1,10 @@
 package dragon.osc.parse.tfm
+
+object Tfm {
+    def includeFromFile(obj: Lang): Lang = obj match {
+        case MapSym(m) => MapSym(m.map(includeInMap))
+        case ListSym(xs) => ListSym(xs.map(includeFromFile))
+        case _ => obj
+    }
+}
+
