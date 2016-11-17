54f51ab30378b5b530f27cd6fc2175c36ce8cf02
anton-k <anton.kholomiov@gmail.com>
2016-11-17 09:54:24 +0300

    include sub-files

--- src/main/scala/parse/Parse.scala
+++ src/main/scala/parse/Parse.scala
@@ -2,10 +2,11 @@ package dragon.osc.parse
 
 import dragon.osc.parse.syntax._
 import dragon.osc.parse.ui._
+import dragon.osc.parse.tfm._
 
 object Parse {
 
     def file(filename: String): Option[Root] =
-        Lang.readFile(filename).flatMap(Read.root.run _)    
+        Lang.readFile(filename).flatMap(x => Read.root.run(Tfm.tfmLang(x)))    
 
 }
--- src/main/scala/parse/Tfm.scala
+++ src/main/scala/parse/Tfm.scala
@@ -1,10 +1,37 @@
 package dragon.osc.parse.tfm
 
+import dragon.osc.parse.syntax._
+
 object Tfm {
+    def tfmLang(obj: Lang): Lang = 
+        includeFromFile(obj)
+
     def includeFromFile(obj: Lang): Lang = obj match {
-        case MapSym(m) => MapSym(m.map(includeInMap))
+        case MapSym(m) => MapSym(m.toList.flatMap(includeInMap).toMap)
         case ListSym(xs) => ListSym(xs.map(includeFromFile))
         case _ => obj
     }
+
+    def includeInMap(p: (String,Lang)): List[(String,Lang)] = 
+        if (p._1 == "#include") {
+            p._2 match {
+                case PrimSym(PrimString(filename)) => includeWholeFile(filename)
+                case MapSym(m) => m.toList match {
+                    case List((field, PrimSym(PrimString(filename)))) => includeFileByField(field, filename)
+                    case _ => Nil
+                }
+                case _ => Nil
+            }
+        } else List(p)
+
+    def insertMap(obj: Lang): List[(String,Lang)] = obj match {
+        case MapSym(m) => m.toList.map({ case (key, value) => (key, includeFromFile(value)) })
+        case _ => Nil
+    }
+
+    def includeWholeFile(filename: String): List[(String,Lang)] = Lang.readFile(filename).map(insertMap).getOrElse(Nil)
+
+    def includeFileByField(field: String, filename: String): List[(String,Lang)] = 
+        Lang.readFile(filename).flatMap(_.getKey(field)).map(insertMap).getOrElse(Nil)
 }
 
